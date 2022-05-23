79f1fb61c90914b2a7a84f14cac1172e6a18389b
Nugine <nugine@foxmail.com>
2022-05-23 15:45:33 +0800

    rdma: lints

--- crates/rdma/src/lib.rs
+++ crates/rdma/src/lib.rs
@@ -1,1 +1,19 @@
-#![deny(clippy::all)]
+#![deny(clippy::all, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::pub_use,
+    clippy::implicit_return,
+    clippy::panic_in_result_fn,
+    clippy::expect_used,
+    clippy::unwrap_used,
+    clippy::wildcard_imports,
+    clippy::unwrap_in_result,
+    clippy::transmute_ptr_to_ptr,
+    clippy::shadow_reuse
+)]
+#![allow(
+    clippy::missing_errors_doc, // TODO
+    clippy::missing_docs_in_private_items, // TODO
+    clippy::missing_panics_doc, // TODO
+)]
