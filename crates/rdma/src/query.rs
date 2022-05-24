968238643386cd3896b2ff42f90b6e66c1bcdcc3
Nugine <nugine@foxmail.com>
2022-05-24 22:56:09 +0800

    rdma: query: DeviceAttr

--- crates/rdma/src/query.rs
+++ crates/rdma/src/query.rs
@@ -2,22 +2,22 @@ use crate::error::from_errno;
 use crate::utils::{box_assume_init, box_new_uninit};
 use crate::Context;
 
-use rdma_sys::ibv_device_attr;
-use rdma_sys::ibv_query_device;
+use rdma_sys::ibv_device_attr_ex;
+use rdma_sys::ibv_query_device_ex;
 
-use std::io;
+use std::{io, ptr};
 
-pub struct DeviceAttr(Box<ibv_device_attr>);
-
-// TODO: ibv_device_attr_ex
+pub struct DeviceAttr(Box<ibv_device_attr_ex>);
 
 impl DeviceAttr {
     #[inline]
     pub fn query(ctx: &Context) -> io::Result<Self> {
-        let mut device_attr = box_new_uninit::<ibv_device_attr>();
         // SAFETY: ffi
         unsafe {
-            let ret = ibv_query_device(ctx.0.ffi_ptr(), device_attr.as_mut_ptr());
+            let mut device_attr = box_new_uninit::<ibv_device_attr_ex>();
+            let context = ctx.0.ffi_ptr();
+            let input = ptr::null();
+            let ret = ibv_query_device_ex(context, input, device_attr.as_mut_ptr());
             if ret > 0 {
                 return Err(from_errno(ret));
             }
@@ -28,7 +28,7 @@ impl DeviceAttr {
     #[inline]
     #[must_use]
     pub fn physical_port_count(&self) -> u32 {
-        self.0.phys_port_cnt.into()
+        self.0.orig_attr.phys_port_cnt.into()
     }
 }
 
@@ -36,13 +36,10 @@ impl DeviceAttr {
 mod tests {
     use super::*;
 
-    use rdma_sys::ibv_device_attr_ex;
-
     use std::mem;
 
     #[test]
     fn track_type_size() {
-        assert_eq!(mem::size_of::<ibv_device_attr>(), 232);
         assert_eq!(mem::size_of::<ibv_device_attr_ex>(), 400);
     }
 }
